a401f70ecd4dba7f80c54a3aef71d7fff05cd937
annaklimenko <annaklimenko5248@gmail.com>
2023-05-04 09:35:13 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,7 @@ public class Main {
         int a = scn.nextInt();
         System.out.print("Enter b: ");
         int b = scn.nextInt();
-        System.out.println("1. Sum\n2. Minus\n3. Multiplication\n4. Деление");
-        int input = scn.nextInt();
-        switch (input) {
-            case 1 -> System.out.println(a + b);
-            case 2 -> System.out.println(a - b);
-            case 3 -> System.out.println(a * b);
-            case 4 -> System.out.println(a / b);
-
-        }
+        System.out.println(a+b);
         System.out.println("Good bye!");
     }
 }
